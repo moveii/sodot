ab1804f91f9f336a4a142d9c66f6f13b56a3397b
Patrick Berger <ber17409@spengergasse.at>
2021-01-15 21:24:48 +0100

    Initial Commit

---
+++ src/test/java/com/moveii/sodot/SodotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveii.sodot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
